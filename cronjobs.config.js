d775314af6c08e25d13260492621c4f75dceb0da
nilpatel <nilpatel62@gmail.com>
2025-08-20 16:52:12 +0530

    Update CHECK_INTERVAL_SEC for more frequent monitoring and refresh state file with latest alert time

--- cronjobs.config.js
+++ cronjobs.config.js
@@ -5,8 +5,7 @@ module.exports = {
             script: 'tcrb_monitor_adql.py',
             autorestart: false,
             watch: false,
-            max_memory_restart: '1G',
-            cron_restart: '0 * * * *',  // Runs every hour
+            max_memory_restart: '1G'
         }
     ]
 };
